b26e45ba9b10830509fa00c77accbe89aef241f2
dlwar3 <dlwarren2@crimson.ua.edu>
2021-12-12 01:07:54 -0600

    interpret classifications of petri net

--- src/plugins/PeNDeSInterpreter/PeNDeSInterpreter.js
+++ src/plugins/PeNDeSInterpreter/PeNDeSInterpreter.js
@@ -54,18 +54,154 @@ define([
      */
     PeNDeSInterpreter.prototype.main = function (callback) {
         // Use this to access core, project, result, logger etc from PluginBase.
-        const self = this;
-
-        // Using the logger.
-        self.logger.debug('This is a debug message.');
-        self.logger.info('This is an info message.');
-        self.logger.warn('This is a warning message.');
-        self.logger.error('This is an error message.');
+        var self = this,
+        activeNode = this.activeNode,
+        core = this.core,
+        logger = this.logger;
 
         // Using the coreAPI to make changes.
         const nodeObject = self.activeNode;
-        self.core.setAttribute(nodeObject, 'name', 'My new obj');
-        self.core.setRegistry(nodeObject, 'position', {x: 70, y: 70});
+        
+        
+
+  this.loadNodeMap(activeNode)
+    .then(function (nodes) {
+      var path;
+      var tree = {};
+      var transitions = {};
+      var places = {};
+      var arcs = {};
+      for (path in nodes) {       
+        tree[path] = {"name": core.getAttribute(nodes[path], 'name'),       // node name
+                      "GUID": core.getGuid(nodes[path]),                    // node guid
+                      "meta": "",                                           // placeholder node meta name
+                      "attributes": {}                                      // placeholder node attribute obj
+                     };
+
+        
+        // find meta node and name
+        let metaNode = core.getMetaType(nodes[path]);
+        if (metaNode != null ) { 
+          tree[path].meta = core.getAttribute(metaNode, 'name');
+          if (tree[path].meta == "Transition")
+          {
+              transitions[path] = nodes[path];
+          }
+          if (tree[path].meta == "Place")
+          {
+              places[path] = nodes[path];
+          }
+          if (tree[path].meta == "Arc")
+          {
+              arcs[path] = nodes[path];
+          }
+        }
+        else {
+          tree[path].meta = null; //account for null value of root
+        }
+        
+        
+        var attr = core.getAttributeNames(nodes[path]);
+        tree[path].attributes = {};
+        for (let name in attr)
+        {         
+          // we want valid attributes that are strings to display, not functions or anything else
+          // ignore script code, not relevant
+          if (typeof attr[name] == "string") {
+            tree[path].attributes[attr[name]] = core.getAttribute(nodes[path], attr[name]);
+          }
+        }
+      }
+    
+        //console.log(JSON.stringify(tree,null,'\t'));
+
+        var pointerPaths = {};
+        for (let arc in arcs)
+        {
+            pointerPaths[arc] = {"src": core.getPointerPath(arcs[arc], "src"), "dst": core.getPointerPath(arcs[arc], "dst")};
+        }
+
+        // is free choice?
+        var inplaces = [];
+        var freeChoice = 1;
+        for (let arc in arcs)
+        {
+            
+            for (let place in places)
+            {   
+                // if a place is a src to a transition,
+                if (pointerPaths[arc].src.includes(place))
+                {
+                    for (let trans in transitions)
+                    {
+                        if (pointerPaths[arc].dst.includes(trans))
+                        {
+                            if (inplaces[place] != undefined)
+                            {
+                                console.log(place);
+                                console.log(inplaces[place]);
+                                freeChoice = 0;
+                            }
+                            else 
+                            {
+                                inplaces[place] = trans;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if (freeChoice)
+        {
+            console.log("Petri net is free-choice.");
+        }
+
+        var outplaces = [];
+        var stateMachine = 1;
+        //is state machine?
+        for (let arc in arcs)
+        {
+            
+            for (let place in places)
+            {   
+                // if a place is a src to a transition,
+                if (pointerPaths[arc].dst.includes(place))
+                {
+                    for (let trans in transitions)
+                    {
+                        if (pointerPaths[arc].src.includes(trans))
+                        {
+                            if (outplaces[trans] != undefined)
+                            {
+                                stateMachine = 0;
+                            }
+                            else 
+                            {
+                                outplaces[trans] = place;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (stateMachine)
+        {
+            console.log("Petri net is state machine.");
+        }
+
+        //is marked graph?
+        var markedGraph = 0;
+        if (freeChoice && stateMachine)
+        {
+            markedGraph = 1;
+            console.log("Petri net is marked graph.");
+        }
+        
+
+        //is workflow net?
+        //TODO: logic to determine workflow net
+
 
 
         // This will save the changes. If you don't want to save;
@@ -80,6 +216,7 @@ define([
                 self.logger.error(err.stack);
                 callback(err, self.result);
             });
+        });
     };
 
     return PeNDeSInterpreter;
